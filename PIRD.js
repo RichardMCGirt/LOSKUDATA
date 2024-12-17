6f1b6df508a455f8929433d54079322d42c34633
Richard McGirt <rmcgirt55@gmail.com>
2024-12-17 08:55:19 -0500

    addes

--- PIRD.js
+++ PIRD.js
@@ -130,7 +130,7 @@ function exportToFinalCounts() {
     console.log('Data successfully stored in sessionStorage.');
 
     // Redirect to finalCounts.html
-    window.location.href = 'finalCounts.html';
+    window.location.href = 'FinalCounts.html';
 }
 
 // Add event listener for exporting to finalCounts.html
